9eb2f8444d007df99529125c7368c5d80ccb0a63
Francesco <francesco.savignano96@gmail.com>
2020-11-11 10:57:39 +0100

    Array di partenza stampato nel dom

---
+++ main.js
@@ -0,0 +1,18 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            todolist: [
+                'cucinare',
+                'portare fuori il cane',
+                'esercitazione VUE'
+            ]
+        },
+        methods: {
+
+        },
+        mounted() {
+
+        }
+    }
+);
