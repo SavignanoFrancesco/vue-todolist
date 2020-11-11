f5c9fa32eea7318314aff45a232dd2655a98bfa8
Francesco <francesco.savignano96@gmail.com>
2020-11-11 11:28:05 +0100

    aggiunta di un todo alla lista tramite click su un bottone

--- main.js
+++ main.js
@@ -2,14 +2,19 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            todolist: [
+            newTodo: '',
+            todoList: [
                 'cucinare',
                 'portare fuori il cane',
                 'esercitazione VUE'
             ]
         },
         methods: {
-
+            addTodo(){
+                this.todoList.push(this.newTodo);
+                this.newTodo='';
+                console.log(this.todoList);
+            }
         },
         mounted() {
 
